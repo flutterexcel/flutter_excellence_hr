784c47a6812fd26a495aa749d773a6f41daff93f
aishwary <kaulaishwary11@gmail.com>
2020-08-26 11:36:44 +0530

    wenday morning commit

---
+++ lib/routes.dart
@@ -0,0 +1,10 @@
+ import 'package:flutter/material.dart';
+// import 'package:screens/home/home_screen.dart';
+ import 'LoginScreen.dart';
+
+final routes = {
+   '/login': (BuildContext context) =>  LoginScreen(),
+  // '/home': (BuildContext context) =>  HomeScreen(),
+
+  '/': (BuildContext context) =>  LoginScreen(),
+};
