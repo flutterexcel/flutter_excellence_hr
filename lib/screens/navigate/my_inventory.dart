5f7e3cc52dc62472ccf68e58b50a7aa56896157b
aishwary <kaulaishwary11@gmail.com>
2020-09-07 19:57:18 +0530

    Dyanamic data added

--- lib/screens/navigate/my_inventory.dart
+++ lib/screens/navigate/my_inventory.dart
@@ -10,7 +10,6 @@ import 'package:flutter_excellence_hr/resources/app_colors.dart';
 class YourInventory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -27,61 +26,65 @@ class YourInventory extends StatelessWidget {
           ],
         ),
       ),
-      drawer: Navigation(),    
-    
-    floatingActionButton: FloatingActionButton.extended(
+      drawer: Navigation(),
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           // Add your onPressed code here!
         },
         label: Text('Assign Device'),
         backgroundColor: AppColors.GREEN_COLOR,
       ),
-      
-
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               ImportantNotesInventory(),
               SizedBox(height: 20),
-             InventoryItems(),
-              SizedBox(height: 50),  
-            Container(
-                      margin: EdgeInsets.fromLTRB(8, 16, 8, 16),
-                      child: Text(
-                        'Device Details',
-                        style: TextStyle(
+              InventoryItems(),
+              SizedBox(height: 50),
+              Container(
+                margin: EdgeInsets.fromLTRB(8, 16, 8, 16),
+                child: Text(
+                  'Device Details',
+                  style: TextStyle(
+                    color: AppColors.LIGHTBLACK_COLOR,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              DropDown(),
+              CommentSection(),
+              InventoryDetails(),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(16, 16, 8, 16),
+                    child: Text(
+                      'Recent Comments',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
                           color: AppColors.LIGHTBLACK_COLOR,
                           fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                          fontWeight: FontWeight.w400),
                     ),
-              DropDown(),        
-              CommentSection(),
-             InventoryDetails() , 
-             SizedBox(height:20),
-             Row(
-               children: [
-                 Container(
-                          margin: EdgeInsets.fromLTRB(16, 16, 8, 16),
-                          child: Text(
-                            'Recent Comments',textAlign:TextAlign.left,
-                            style: TextStyle(
-                              color: AppColors.LIGHTBLACK_COLOR,
-                              fontSize: 25,fontWeight: FontWeight.w400
-                            ),
-                          ),
-                        ),
-               ],
-             ), 
-             Divider(height: 2,endIndent: 16,indent: 16, thickness:2, color: AppColors.COMMENT_ICON,),          
-             CommentList(),
-             SizedBox(height:50)
+                  ),
+                ],
+              ),
+              Divider(
+                height: 2,
+                endIndent: 16,
+                indent: 16,
+                thickness: 2,
+                color: AppColors.COMMENT_ICON,
+              ),
+              CommentList(),
+              SizedBox(height: 50)
             ],
           ),
         ),
       ),
     );
-    }
+  }
 }
