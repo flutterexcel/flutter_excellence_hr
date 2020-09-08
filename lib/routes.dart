ad4641c88ec44f0250e224843aeb9174bfc96662
aishwary <kaulaishwary11@gmail.com>
2020-09-08 18:49:49 +0530

    overview ui

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,5 +3,6 @@ import 'screens/show_inventory.dart';
 import 'package:flutter_excellence_hr/screens/navigate/my_inventory.dart';
 import 'package:flutter_excellence_hr/screens/navigate/overview.dart';
 final routes = {
-  '/my_inventory': (BuildContext context) => Overview(),
+  '/my_inventory': (BuildContext context) => ShowInventory(),
+  '/overview': (BuildContext context) => Overview(),
 };
