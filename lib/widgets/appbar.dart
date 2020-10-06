8c318189a05a573297fed6fc5bc1cd74e496e12b
Shaktiexcellence <shakti@excellencetechnologies.in>
2020-10-06 20:18:24 +0530

    AppBar dyanamic image added

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,27 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_excellence_hr/model/profile/ProfileDetails.dart';
+import 'package:flutter_excellence_hr/services/profile/profile.dart';
 
 class AppBarWidget extends StatefulWidget {
-  final String pageName;
-  const AppBarWidget({Key key, this.pageName}) : super(key: key);
-
+  final pageName;
+  AppBarWidget({this.pageName});
   @override
-  _AppBarWidgetState createState() => _AppBarWidgetState();
+  _AppBarWidgetState createState() => _AppBarWidgetState(pageName: pageName);
 }
 
 class _AppBarWidgetState extends State<AppBarWidget> {
+  String pageName;
+  _AppBarWidgetState({this.pageName});
+
+  Profile api = new Profile();
+  ProfileDetails profileDetails;
+  bool profileImg = false;
+  _getProfile() async {
+    return await api.getprofile().then((value) {
+      profileDetails = value;
+      setState(() {
+        profileImg = true;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getProfile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(widget.pageName,
+        Text(pageName,
             style: TextStyle(
                 color: Colors.white,
                 fontSize: 20,
                 fontFamily: 'OpenSans',
                 fontWeight: FontWeight.bold)),
-        CircleAvatar(
-            radius: 20, backgroundImage: AssetImage('assets/images/person.jpg'))
+        profileImg
+            ? CircleAvatar(
+                radius: 20,
+                backgroundImage: NetworkImage(
+                    profileDetails.data.userProfileDetail.profileImage))
+            : Center()
       ],
     );
   }
