078c3f984bea3f6177a4199e3c18cfd2ff12876a
Shaktiexcellence <shakti@excellencetechnologies.in>
2020-10-12 13:03:23 +0530

    fix

--- lib/widgets/attendance_wigets/complete.dart
+++ lib/widgets/attendance_wigets/complete.dart
@@ -8,12 +8,12 @@ class CompletePending extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final completePercent = .9;
-    final pendingPercent = .1;
-    // final completePercent = (monthSummary.secondsCompletedWorkingHours) /
-    //     (monthSummary.secondsActualWorkingHours);
-    // final pendingPercent = (monthSummary.secondsPendingWorkingHours) /
-    //     (monthSummary.secondsActualWorkingHours);
+     final completePercent = (monthSummary.secondsCompletedWorkingHours) /
+         (monthSummary.secondsActualWorkingHours)>.9?0:(monthSummary.secondsCompletedWorkingHours) /
+         (monthSummary.secondsActualWorkingHours);
+     final pendingPercent = (monthSummary.secondsPendingWorkingHours) /
+         (monthSummary.secondsActualWorkingHours)>.9?0:(monthSummary.secondsPendingWorkingHours) /
+         (monthSummary.secondsActualWorkingHours);
     return Card(
       margin: EdgeInsets.fromLTRB(8, 16, 8, 16),
       child: Column(
