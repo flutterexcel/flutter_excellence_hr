6fcfb1ebe5ea3b4e39bf33075015012cd00dc4c9
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-11-04 17:48:49 +0530

    update before pull

---
+++ test_driver/loginvalidationtest/validation.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:flutter_excellence_hr/main.dart'as loginValidation;
+
+void main() {
+  enableFlutterDriverExtension();
+  loginValidation.main();
+}
