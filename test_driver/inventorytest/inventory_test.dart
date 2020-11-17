5121ae5e2ab5471ab2e7d0d6526afe7d179a8da0
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-11-17 15:30:56 +0530

    update inventory and profile

--- test_driver/inventorytest/inventory_test.dart
+++ test_driver/inventorytest/inventory_test.dart
@@ -1,14 +1,12 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
+import '../inventorytest.dart';
 import '../logintest.dart';
-import '../logouttest.dart';
-import '../profiletest.dart';
 
 void main() {
   FlutterDriver driver;
-
-  group('check app flow/', () {
+  group('check inventory page/', () {
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
@@ -18,9 +16,6 @@ void main() {
       }
     });
     loginTest(driver);
-    profileTest(driver);
-    loginTest(driver);
-    
-    logOut(driver);
+    inventorytest(driver);
   });
 }
