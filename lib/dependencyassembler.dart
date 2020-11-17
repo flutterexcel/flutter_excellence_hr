c3b738f445d67b8a5838320b4aca3ce2c9e1733c
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-11-17 12:18:58 +0530

    update login test

--- lib/dependencyassembler.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_excellence_hr/services/authentication_services.dart';
-import 'package:flutter_excellence_hr/services/login.dart';
-import 'package:get_it/get_it.dart';
-
-GetIt dependencyAssembler = GetIt.instance;
-
-void setupDependencyAssembler() {
-  dependencyAssembler.registerLazySingleton(() => LoginAuthenticationService());
-}
