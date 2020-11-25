5307814ff8656ed564304e1d1477c92bdaf54bf0
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-11-25 12:32:56 +0530

    update auto login issue complete

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,6 +1,5 @@
 library my_prj.globals;
 
-import 'package:shared_preferences/shared_preferences.dart';
 
 bool testingActive = false; //default value-false
 bool loginActive = true; //default value-true
