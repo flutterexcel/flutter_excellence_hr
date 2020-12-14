ed72c63db3cd83c1ca7058bccb859065afc12f5e
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-12-14 19:46:46 +0530

    update apply rh leave

---
+++ lib/widgets/my_leaves_widgets/apply_rh_leave.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_excellence_hr/resources/app_colors.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
+
+class ApplyRhLeave extends StatelessWidget {
+  String rhName;
+  String date;
+  ApplyRhLeave(this.rhName, this.date);
+  final reason = TextEditingController();
+  final RoundedLoadingButtonController _btnController =
+      new RoundedLoadingButtonController();
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+         height: 300,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Text(
+              "RH Name: $rhName",
+              style: TextStyle(
+                  color: AppColors.THEME_COLOR,
+                  fontSize: 16,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold
+                  ),
+            ),
+            Text(
+              "Date: $date",
+              style: TextStyle(
+                  color: AppColors.THEME_COLOR,
+                  fontSize: 16,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Reason for RH: ",
+              style: TextStyle(
+                
+                  color: AppColors.THEME_COLOR,
+                  fontSize: 16,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextFormField(
+                key: Key('reasonKey'),
+                enabled: true,
+                controller: reason,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "",
+                    labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'OpenSans',
+                        fontSize: 16)),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              margin: EdgeInsets.all(8),
+              width: 80,
+              child: RoundedLoadingButton(
+                color: AppColors.BTN_BLACK_COLOR,
+                width: 110,
+                borderRadius: 10,
+                controller: _btnController,
+                onPressed: () {
+                  // if (!validateTextField(reason.text))
+                  //   _btnController.stop();
+                  // else if (!validateEntryField(entry.text))
+                  //   _btnController.stop();
+                  // else if (!validateExitField(exit.text))
+                  //   _btnController.stop();
+                  // else {
+                  //   _doUpdate();
+                  //   Timer(Duration(seconds: 5), () {
+                  //     Navigator.pop(context);
+                  //   });
+                  // }
+                },
+                child: Text('Apply Rh', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
