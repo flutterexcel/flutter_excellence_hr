05699383362ef798e2ccc2914b37b18cf2f6cf14
Shaktiexcellence <shakti@excellencetechnologies.in>
2020-12-30 19:31:32 +0530

    Add day n time

--- lib/widgets/timesheet_widgets/header.dart
+++ lib/widgets/timesheet_widgets/header.dart
@@ -2,20 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_excellence_hr/resources/app_colors.dart';
 
 class Header extends StatelessWidget {
+  DateTime now = DateTime.now();
+  double week;
+  DateTime next = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Container(
         color: AppColors.BLUE_COLOR,
         margin: EdgeInsets.all(8),
         child: ListTile(
-          leading: Icon(
-            Icons.arrow_left,
-            color: Colors.white,
+          leading: InkWell(
+            onTap: () {},
+            child: Icon(
+              Icons.arrow_left,
+              color: Colors.white,
+            ),
           ),
-          title: Text(
-            'WEEK 2 (DECEMBER)',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
+          title: InkWell(
+            onTap: () {
+              week = now.day / 7 + 1;
+              next = next.add(Duration(days: 1));
+              print(" the next day is  " +
+                  next.day.toString() +
+                  "Weekday is " +
+                  next.weekday.toString());
+              print("The day is " +
+                  now.day.toString() +
+                  "month is  " +
+                  now.month.toString());
+              print(now.month);
+              print("The week is " + week.toStringAsFixed(0));
+            },
+            child: Text(
+              'WEEK 2 (DECEMBER)',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
+            ),
           ),
           trailing: Icon(
             Icons.arrow_right,
