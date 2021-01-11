324381414f581af4ca913f7161fbb694b73540cc
Shaktiexcellence <shakti@excellencetechnologies.in>
2021-01-11 15:33:42 +0530

    timesheet submit complete

--- lib/screens/weekly_timesheet/timesheet.dart
+++ lib/screens/weekly_timesheet/timesheet.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_excellence_hr/model/timesheet/submit_daily_report.dart';
+import 'package:flutter_excellence_hr/model/timesheet/submit_timesheet.dart';
 import 'package:flutter_excellence_hr/model/timesheet/timesheet.dart';
 import 'package:flutter_excellence_hr/model/timesheet/timesheetupdated.dart';
 import 'package:flutter_excellence_hr/model/tmsreport/tmsreport.dart';
 import 'package:flutter_excellence_hr/resources/app_colors.dart';
 import 'package:flutter_excellence_hr/screens/navigate/navigation.dart';
 import 'package:flutter_excellence_hr/services/timesheet/submit_timesheet_daily.dart';
+import 'package:flutter_excellence_hr/services/timesheet/submit_weekly_report.dart';
 import 'package:flutter_excellence_hr/services/timesheet/timesheet.dart';
 import 'package:flutter_excellence_hr/services/timesheet/updatetimesheet.dart';
 import 'package:flutter_excellence_hr/services/tmsreport/tmsreport.dart';
@@ -32,21 +34,28 @@ class _WeeklyTimeSheetState extends State<TimeSheetUI> {
   final comment = TextEditingController();
   TMSReportService apireport = TMSReportService();
   TMSReport tmsReport;
+  SubmitTimeSheet submitTimeSheet;
+  TimeSheetWeeklyService apiWeekly = TimeSheetWeeklyService();
+  String note =
+      """Are you sure want to submit as timesheet,cannot be changed once submit""";
   final DateFormat formatter = DateFormat('yyyy-MM-dd');
   TimeSheetService api = TimeSheetService();
   TimeSheet timeSheet;
   TimeSheetDailyService apidaily = TimeSheetDailyService();
   bool totalTimeValidate = false;
   bool commentValidate = false;
-
+  SubmitTimeSheet submitWeeklyTimeSheet;
   SubmitDailyReport submitDailyReport;
   TimeSheetUpdateService apiUpdateTimeSheet = TimeSheetUpdateService();
   TimeSheetUpdated timeSheetUpdated;
   final RoundedLoadingButtonController _btnController =
       new RoundedLoadingButtonController();
+  final RoundedLoadingButtonController _btnOkController =
+      new RoundedLoadingButtonController();
   bool yourTimesheet = false;
   bool yourTMSReport = false;
   bool yourDailyReport = false;
+  bool enableContent = true;
   DateTime now = DateTime.now();
   DateTime firstDayOfTheweek;
   int index;
@@ -54,7 +63,7 @@ class _WeeklyTimeSheetState extends State<TimeSheetUI> {
 
   _getTimeSheet() async {
     firstDayOfTheweek = now.subtract(new Duration(days: now.weekday - 1));
-    String formatted = formatter.format(firstDayOfTheweek);
+    String formatted = "2020-10-12"; //formatter.format(firstDayOfTheweek);
     return await api.getTimesheet(fromDate: formatted).then((value) {
       timeSheet = value;
       setState(() {
@@ -74,6 +83,24 @@ class _WeeklyTimeSheetState extends State<TimeSheetUI> {
         yourDailyReport = true;
       });
       _doReset();
+    });
+  }
+
+  void _submitWeeklyreport() async {
+    String formatted = "2020-10-12"; //formatter.format(firstDayOfTheweek);
+    return await apiWeekly.sentWeeklyTimesheet(date: formatted).then((value) {
+      // if (submitWeeklyTimeSheet.error == 1) {
+      //   _btnOkController.error();
+      //   _btnOkController.reset();
+      // } else {
+      _btnOkController.success();
+      setState(() {
+        enableContent = false;
+      });
+      Timer(Duration(seconds: 2), () {
+        _btnOkController.reset();
+        Navigator.of(context).pop();
+      });
     });
   }
 
@@ -315,6 +342,144 @@ class _WeeklyTimeSheetState extends State<TimeSheetUI> {
           });
     }
 
+    submitDialog() {
+      return showDialog(
+          context: context,
+          builder: (BuildContext c) {
+            return Dialog(
+              child: Container(
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.all(16),
+                        child: Text(note,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: AppColors.LIGHTBLACK_COLOR))),
+                    SizedBox(height: 10),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 150,
+                            color: Colors.grey,
+                            child: FlatButton(
+                                height: 50,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0)),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("Cancel",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white))),
+                          ),
+                          SizedBox(width: 15),
+                          RoundedLoadingButton(
+                              width: 150,
+                              borderRadius: 5,
+                              controller: _btnOkController,
+                              onPressed: () {
+                                _submitWeeklyreport();
+                                _getTimeSheet();
+                                // Timer(Duration(seconds: 5), () {
+                                //   Navigator.pop(context);
+                                // });
+                              },
+                              color: Colors.green[300],
+                              child: Text('Add',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white))),
+                        ])
+                  ],
+                ),
+              ),
+            );
+          });
+    }
+
+    Widget contentFeild() {
+      return Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(16.0),
+          child: GridView.builder(
+            itemCount: timeSheet.data.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+            itemBuilder: (BuildContext context, index) {
+              return Column(
+                children: [
+                  Container(
+                      width: MediaQuery.of(context).size.width * .25,
+                      //  margin: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                      child: Text(
+                        timeSheet.data[index].day,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'SourceSans',
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  (timeSheet.data[index].dayType == "NON_WORKING_DAY")
+                      ? Container(
+                          color: Colors.yellowAccent,
+                          width: MediaQuery.of(context).size.width * .25,
+                          margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
+                          padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
+                          child: Text(timeSheet.data[index].date,
+                              style: TextStyle(
+                                  fontFamily: 'OpenSans', fontSize: 14)))
+                      : InkWell(
+                          onTap: () {
+                            print("Today date is >>>>>>>>>" +
+                                timeSheet.data[index].fullDate.toString());
+                            customDialog(
+                                day: timeSheet.data[index].day.toString(),
+                                date: timeSheet.data[index].fullDate);
+                          },
+                          child: Container(
+                              color: Colors.grey[300],
+                              width: MediaQuery.of(context).size.width * .25,
+                              margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
+                              padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
+                              child: Text(timeSheet.data[index].date,
+                                  style: TextStyle(
+                                      fontFamily: 'OpenSans', fontSize: 14))),
+                        ),
+                  (timeSheet.data[index].dayType == "NON_WORKING_DAY")
+                      ? Container(
+                          color: Colors.yellowAccent,
+                          width: MediaQuery.of(context).size.width * .25,
+                          //  margin: EdgeInsets.fromLTRB(8, 0, 1, 8),
+                          padding: EdgeInsets.fromLTRB(8, 0, 0, 8),
+                          child: Text(timeSheet.data[index].dayText,
+                              style: TextStyle(
+                                  fontFamily: 'OpenSans', fontSize: 14)))
+                      : InkWell(
+                          onTap: () {
+                            customDialog(
+                                day: timeSheet.data[index].day.toString(),
+                                date: timeSheet.data[index].fullDate);
+                          },
+                          child: Container(
+                              color: Colors.grey[300],
+                              width: MediaQuery.of(context).size.width * .25,
+                              padding: EdgeInsets.fromLTRB(8, 0, 0, 8),
+                              child: Text(
+                                  timeSheet.data[index].totalHours.toString() +
+                                      " - Hours",
+                                  style: TextStyle(
+                                      fontFamily: 'OpenSans', fontSize: 14))),
+                        ),
+                ],
+              );
+            },
+          ));
+    }
+
     return yourTimesheet
         ? Scaffold(
             backgroundColor: AppColors.BACKGROUND_COLOR,
@@ -322,109 +487,22 @@ class _WeeklyTimeSheetState extends State<TimeSheetUI> {
             drawer: Navigation(),
             floatingActionButton: FloatingActionButton.extended(
                 onPressed: () {
-                  // Add your onPressed code here!
+                  submitDialog();
+                  // _submitWeeklyreport();
+                  // _getTimeSheet();
+                  setState(() {
+                    //   enableContent = false;
+                  });
                 },
                 label: Text('Submit Weekly Timesheet'),
                 backgroundColor: AppColors.BTN_BLACK_COLOR),
             body: SafeArea(
                 child: SingleChildScrollView(
               child: StickyHeader(
-                header: Header(),
-                content: Container(
-                    height: MediaQuery.of(context).size.height,
-                    padding: EdgeInsets.all(16.0),
-                    child: GridView.builder(
-                      itemCount: timeSheet.data.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 4.0,
-                          mainAxisSpacing: 4.0),
-                      itemBuilder: (BuildContext context, index) {
-                        return Column(
-                          children: [
-                            Container(
-                                width: MediaQuery.of(context).size.width * .25,
-                                //  margin: EdgeInsets.fromLTRB(16, 8, 16, 8),
-                                child: Text(
-                                  timeSheet.data[index].day,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontFamily: 'SourceSans',
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold),
-                                )),
-                            (timeSheet.data[index].dayType == "NON_WORKING_DAY")
-                                ? Container(
-                                    color: Colors.yellowAccent,
-                                    width:
-                                        MediaQuery.of(context).size.width * .25,
-                                    margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
-                                    padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
-                                    child: Text(timeSheet.data[index].date,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans',
-                                            fontSize: 14)))
-                                : InkWell(
-                                    onTap: () {
-                                      print("Today date is >>>>>>>>>" +
-                                          timeSheet.data[index].fullDate
-                                              .toString());
-                                      customDialog(
-                                          day: timeSheet.data[index].day
-                                              .toString(),
-                                          date: timeSheet.data[index].fullDate);
-                                    },
-                                    child: Container(
-                                        color: Colors.grey[300],
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                .25,
-                                        margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
-                                        padding:
-                                            EdgeInsets.fromLTRB(8, 8, 8, 8),
-                                        child: Text(timeSheet.data[index].date,
-                                            style: TextStyle(
-                                                fontFamily: 'OpenSans',
-                                                fontSize: 14))),
-                                  ),
-                            (timeSheet.data[index].dayType == "NON_WORKING_DAY")
-                                ? Container(
-                                    color: Colors.yellowAccent,
-                                    width:
-                                        MediaQuery.of(context).size.width * .25,
-                                    //  margin: EdgeInsets.fromLTRB(8, 0, 1, 8),
-                                    padding: EdgeInsets.fromLTRB(8, 0, 0, 8),
-                                    child: Text(timeSheet.data[index].dayText,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans',
-                                            fontSize: 14)))
-                                : InkWell(
-                                    onTap: () {
-                                      customDialog(
-                                          day: timeSheet.data[index].day
-                                              .toString(),
-                                          date: timeSheet.data[index].fullDate);
-                                    },
-                                    child: Container(
-                                        color: Colors.grey[300],
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                .25,
-                                        padding:
-                                            EdgeInsets.fromLTRB(8, 0, 0, 8),
-                                        child: Text(
-                                            timeSheet.data[index].totalHours
-                                                    .toString() +
-                                                " - Hours",
-                                            style: TextStyle(
-                                                fontFamily: 'OpenSans',
-                                                fontSize: 14))),
-                                  ),
-                          ],
-                        );
-                      },
-                    )),
-              ),
+                  header: Header(),
+                  content: enableContent
+                      ? contentFeild()
+                      : AbsorbPointer(child: contentFeild())),
             )),
           )
         : Scaffold(
--- lib/services/timesheet/submit_weekly_report.dart
+++ lib/services/timesheet/submit_weekly_report.dart
@@ -1,28 +1,28 @@
 import 'dart:async';
 import 'dart:convert';
 import '../post.dart';
-import '../../model/timesheet/timesheet.dart';
+import '../../model/timesheet/submit_timesheet.dart';
 import '../../app_config.dart';
 import '../storage_service.dart';
 
-class TimeSheetService {
+class TimeSheetWeeklyService {
   Post _post = Post();
-  Future<TimeSheet> getTimesheet({String fromDate}) async {
+  Future sentWeeklyTimesheet({String date}) async {
     final prodUrl = await AppConfig.forEnvironment('prod', 'apiUrl');
     var token = StorageUtil.getUserToken();
-   
     final apiUrl = prodUrl.baseUrl;
     Map data = {
-      "action": "get_user_timesheet",
+      "action": "submit_timesheet",
       "token": token,
-      "from_date": fromDate,
+      "from_date": date,
       "user_id": StorageUtil.getUserId()
     };
+
     return _post
         .post(apiUrl, body: json.encode(data))
         .then((dynamic res) async {
       if (res["error"] >= 1) throw new Exception(res["data"]["message"]);
-      return TimeSheet.fromJson(res);
+      return SubmitTimeSheet.fromJson(res);
     });
   }
 }
